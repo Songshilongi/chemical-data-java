2160c7e87bee196842f071e8c3ef3049c3f4a985
Ice, Song <songshilong0503@163.com>
2025-03-26 15:19:43 +0800

    feat: 注册和登录Controller

---
+++ service/user-service/src/main/java/com/songshilong/service/user/controller/UserLoginController.java
@@ -0,0 +1,13 @@
+package com.songshilong.service.user.controller;
+
+/**
+ * @BelongsProject: chemical-data-java
+ * @BelongsPackage: com.songshilong.service.user.controller
+ * @Author: Ice, Song
+ * @CreateTime: 2025-03-26  15:16
+ * @Description: UserLoginController
+ * @Version: 1.0
+ */
+public class UserLoginController {
+
+}
---
+++ service/user-service/src/main/java/com/songshilong/service/user/controller/UserRegisterController.java
@@ -0,0 +1,19 @@
+package com.songshilong.service.user.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @BelongsProject: chemical-data-java
+ * @BelongsPackage: com.songshilong.service.user.controller
+ * @Author: Ice, Song
+ * @CreateTime: 2025-03-26  15:16
+ * @Description: UserRegisterController
+ * @Version: 1.0
+ */
+@RestController
+public class UserRegisterController {
+
+
+
+
+}
