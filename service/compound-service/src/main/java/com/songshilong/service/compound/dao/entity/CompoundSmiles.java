d177c4d7cccc485d62afd1eb582068638be3740d
Ice, Song <ice.song@sap.com>
2025-05-08 22:21:19 +0800

    feat: update

--- service/compound-service/src/main/java/com/songshilong/service/compound/dao/entity/CompoundSmiles.java
+++ service/compound-service/src/main/java/com/songshilong/service/compound/dao/entity/CompoundSmiles.java
@@ -1,4 +1,4 @@
-package com.songshilong.service.compound.dao;
+package com.songshilong.service.compound.dao.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
